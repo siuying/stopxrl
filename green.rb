113582992f95251937f5d28f420c57481a060249
siuying <siu.ying@gmail.com>
2009-06-26 00:22:11 +0800

    remove the automigrate add index

--- green.rb
+++ green.rb
@@ -6,7 +6,6 @@ require 'sinatra/base'
 require 'lib/green'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{FileUtils.pwd}/green.db")
-Tweet.first rescue DataMapper.auto_migrate!
 
 class Green < Sinatra::Default
   register Sinatra::Green::Controller
--- lib/green/model.rb
+++ lib/green/model.rb
@@ -1,10 +1,10 @@
 class Tweet
   include DataMapper::Resource
   property  :id,                Serial
-  property  :from_user,         String,  :length => 50
-  property  :to_user,           String,  :length => 50
+  property  :from_user,         String,       :length => 50
+  property  :to_user,           String,       :length => 50
   property  :created_at,        DateTime
-  property  :profile_image_url, String, :length => 256
-  property  :text,              String, :length => 140
-  property  :twitter_id,        BigDecimal
+  property  :profile_image_url, String,       :length => 256
+  property  :text,              String,       :length => 140
+  property  :twitter_id,        BigDecimal,   :unique_index => true
 end

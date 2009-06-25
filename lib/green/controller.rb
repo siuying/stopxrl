000e908a5ceadee90f5f10ba9e36223018a65f23
siuying <siu.ying@gmail.com>
2009-06-25 21:50:41 +0800

    use _BLANK for link

--- lib/green/controller.rb
+++ lib/green/controller.rb
@@ -85,9 +85,9 @@ module Sinatra
         # link hashtag
         # link user
         def prep(text)
-          text = text.gsub /(http[s]?:\/\/[^ \t]+)[ \t]?/, "<a href=\"\\1\">\\1</a> "
-          text = text.gsub /#([^ \t]+)[ \t]?/, "<a href=\"http://search.twitter.com/search?tag=\\1\">#\\1</a> "
-          text = text.gsub /@([^ \t]+)[ \t]?/, "<a href=\"http://twitter.com/\\1\">@\\1</a> "
+          text = text.gsub /(http[s]?:\/\/[^ \t]+)[ \t]?/, "<a target=\"_BLANK\" href=\"\\1\">\\1</a> "
+          text = text.gsub /#([^ \t]+)[ \t]?/, "<a target=\"_BLANK\" href=\"http://search.twitter.com/search?tag=\\1\">#\\1</a> "
+          text = text.gsub /@([^ \t]+)[ \t]?/, "<a target=\"_BLANK\" href=\"http://twitter.com/\\1\">@\\1</a> "
         end 
       end
     end
